5e2a63dd7639134d0740e6f4131e68f68efac086
respana8 <57999297+respana8@users.noreply.github.com>
2024-05-27 20:03:57 +0200

    Add files via upload

---
+++ UF6/Model BD Gestió de Cinema/ConnexioBaseDades.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnexioBaseDades {
+    private static final String URL = "jdbc:mysql//localhost:3306/cinema";
+    private static final String USUARI = "root";
+    private static final String CONTRASENYA = "";
+    public static Connection getConnexio() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return DriverManager.getConnection(URL, USUARI, CONTRASENYA);
+    }
+    public static void main(String[] args) {
+        try {
+            Connection conn = getConnexio();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
