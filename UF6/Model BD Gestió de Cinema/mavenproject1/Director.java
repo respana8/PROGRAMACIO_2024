66580cf66ad11cc6e6d3fa89708c612d71ef6a4e
respana8 <57999297+respana8@users.noreply.github.com>
2024-05-31 00:16:07 +0200

    Add files via upload

---
+++ UF6/Model BD Gestió de Cinema/mavenproject1/Director.java
@@ -0,0 +1,14 @@
+public class Director {
+    private int id;
+    private String nom;
+    private String cognom;
+
+    public Director(int id, String nom, String cognom) {
+        this.id = id;
+        this.nom = nom;
+        this.cognom = cognom;
+    }
+    public int getId() { return id; }
+    public String getNom() { return nom; }
+    public String getCognom() { return cognom; }
+}
